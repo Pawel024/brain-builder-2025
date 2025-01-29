48955bbccff3ee68bfb6193f59a2bc395c0f95b6
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2025-01-29 18:27:48 +0100

    Rough first version of exercise1.1

--- src/otherTasks.js
+++ src/otherTasks.js
@@ -4,10 +4,10 @@ import { Flex, Theme, Box, Heading, Separator, Text, Checkbox } from '@radix-ui/
 import Header from './common/header';
 import * as Slider from '@radix-ui/react-slider';
 
+import { renderLinReg } from './utils/otherTasks/plottingUtils'
+import renderDataMatrix from './utils/otherTasks/matrixUtils';
 import renderEmissions from './utils/otherTasks/emissionUtils';
 
-import renderMatrix from './utils/otherTasks/matrixUtils';
-import { RadialLinearScale } from 'chart.js';
 import { _ } from 'ajv';
 
 
@@ -76,9 +76,9 @@ class BackendTask extends OriginalTask {
             this.ws = new WebSocket(`wss://${this.props.host}/ws/${this.props.userId}/`);
         } else {
             this.ws = null;
-            if (this.props.type === 'ManualMatrix') {
-            this.setState({ view: renderMatrix(5, 3) });
-            }
+            // if (this.props.type === 'ManualMatrix') {
+            // this.setState({ view: renderMatrix(5, 3) });
+            // }
         }
     }
 
@@ -153,15 +153,15 @@ class BackendTask extends OriginalTask {
         };
       }
 
-    handleMatrixChange = (value, whichIn) => {
-        if (whichIn === 1) {
-            this.setState({ in1: value[0] });
-            this.setState({ view: renderMatrix(value[0], this.state.in2) })
-        } else {
-            this.setState({ in2: value[0] });
-            this.setState({ view: renderMatrix(this.state.in1, value[0]) })
-        }
-    }
+    // handleMatrixChange = (value, whichIn) => {
+    //     if (whichIn === 1) {
+    //         this.setState({ in1: value[0] });
+    //         this.setState({ view: renderMatrix(value[0], this.state.in2) })
+    //     } else {
+    //         this.setState({ in2: value[0] });
+    //         this.setState({ view: renderMatrix(this.state.in1, value[0]) })
+    //     }
+    // }
 
     handleAngleChange = this.throttle((value) => {
         this.setState({ in1: value[0] });
@@ -282,39 +282,39 @@ class BackendTask extends OriginalTask {
             </Slider.Root>
         );
         
-        const nObjectsSlider = (
-            <Slider.Root
-                className="SliderRoot"
-                defaultValue={[5]}
-                onValueChange={(value) => this.handleMatrixChange(value, 1)}
-                min={2}
-                max={20}
-                step={1}
-                style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
-            >
-            <Slider.Track className="SliderTrack" style={{ height: 3 }}>
-                <Slider.Range className="SliderRange" />
-              </Slider.Track>
-              <Slider.Thumb className="SliderThumb" aria-label="nObjects" />
-            </Slider.Root>
-        );
+        // const nObjectsSlider = (
+        //     <Slider.Root
+        //         className="SliderRoot"
+        //         defaultValue={[5]}
+        //         onValueChange={(value) => this.handleMatrixChange(value, 1)}
+        //         min={2}
+        //         max={20}
+        //         step={1}
+        //         style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+        //     >
+        //     <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+        //         <Slider.Range className="SliderRange" />
+        //       </Slider.Track>
+        //       <Slider.Thumb className="SliderThumb" aria-label="nObjects" />
+        //     </Slider.Root>
+        // );
         
-        const nFeaturesSlider = (
-            <Slider.Root
-                className="SliderRoot"
-                defaultValue={[3]}
-                onValueChange={(value) => this.handleMatrixChange(value, 2)}
-                min={1}
-                max={10}
-                step={1}
-                style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
-            >
-            <Slider.Track className="SliderTrack" style={{ height: 3 }}>
-                <Slider.Range className="SliderRange" />
-              </Slider.Track>
-              <Slider.Thumb className="SliderThumb" aria-label="nFeatures" />
-            </Slider.Root>
-        );
+        // const nFeaturesSlider = (
+        //     <Slider.Root
+        //         className="SliderRoot"
+        //         defaultValue={[3]}
+        //         onValueChange={(value) => this.handleMatrixChange(value, 2)}
+        //         min={1}
+        //         max={10}
+        //         step={1}
+        //         style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+        //     >
+        //     <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+        //         <Slider.Range className="SliderRange" />
+        //       </Slider.Track>
+        //       <Slider.Thumb className="SliderThumb" aria-label="nFeatures" />
+        //     </Slider.Root>
+        // );
 
         return (
             <Box style={{ flex:1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: window.innerHeight-52, padding:'30px 50px',  }}>
@@ -328,7 +328,7 @@ class BackendTask extends OriginalTask {
                     (this.props.type === 'ManualLinReg' || this.props.type === 'ManualPCA') ? weightSlider
                     : this.props.type === 'Manual3DPCA' ? angleSlider
                     : this.props.type === 'ManualPolyReg' ? orderSlider 
-                    : this.props.type === 'ManualMatrix' ? nObjectsSlider
+                    // : this.props.type === 'ManualMatrix' ? nObjectsSlider
                     : null}
                 </div>
 
@@ -337,7 +337,7 @@ class BackendTask extends OriginalTask {
                 <div>{this.state.in2Name}: {this.state.in2}</div>
                 <div className="slider" style={{ marginTop:10   , height:50, display: 'flex', justifyContent: 'center' }}>
                     {this.props.type === 'ManualLinReg' ? biasSlider
-                    : this.props.type === 'ManualMatrix' ? nFeaturesSlider
+                    // : this.props.type === 'ManualMatrix' ? nFeaturesSlider
                     : null}
                 </div>
                 </>
@@ -375,14 +375,17 @@ class OtherTask extends Component {
     constructor(props) {
         super(props);
 
-        if (this.props.type === 'ManualMatrix') {
-            this.animation = DataMatrixAnimation
+        if (this.props.type === 'ManualLinReg') {
+            this.animation = renderLinReg
+        } else if (this.props.type === 'ManualMatrix') {
+            this.animation = renderDataMatrix
         } else {
             alert("Function not implemented yet")
         }
+
         this.animationWindowRef = React.createRef();
         this.state = {
-            animationStates: {}, // TODO: figure out if there is an easier way to force a rerender
+            animationStates: {}, // changing one of these in the animation function causes a rerender
             animationWindowWidth: 100, // TODO: update default value
             animationWindowHeight: 100, // TODO: update default value
         }
@@ -440,161 +443,6 @@ class OtherTask extends Component {
         </Theme>
         );
     }
-}
-
-function DataMatrixAnimation(width, height, states, stateSetter) {
-    let inputOptions = ["Aircraft Type", "Origin", "Destination", "Distance (km)"];  // "Airline", "Number of Passengers", ...
-    let allInputData = [
-        ['A220', '787', '737'], // 'E190'
-        ['Milan', 'Amsterdam', 'Eindhoven'], // 'London' 
-        ['Amsterdam', 'New York', 'Malaga'], // 'Rotterdam'
-        [820, 5860, 1820] // 300
-    ];
-    if (states['inputData']) {
-    } else {
-        states['inputData'] = [[]]
-        stateSetter('inputData', [[]]);
-    }
-    if (states['features']) {
-    } else {
-        states['features'] = []
-        stateSetter('features', []);
-    }
-
-    let outputOptions = ["Manufacturer", "Flight Time (mins)"];
-    let allOutputData = [
-        ['Airbus', 'Boeing', 'Boeing'], // 'Embraer'
-        [110, 500, 175] // 55
-    ];
-    if (states['outputData']) {
-    } else {
-        states['outputData'] = [[]]
-        stateSetter('outputData', [[]]);
-    }
-    if (states['targets']) {
-    } else {
-        states['targets'] = []
-        stateSetter('targets', []);
-    }
-
-    const initialCheckboxValues = [...inputOptions, ...outputOptions].reduce((acc, key) => {
-        acc[key] = false;  // initialize all as false
-        return acc;
-      }, {});
-    if (states['checkboxValues']) {
-    } else {
-        states['checkboxValues'] = initialCheckboxValues
-        stateSetter('checkboxValues', initialCheckboxValues);
-    }
-
-    //const [convertToNumbers, setConvertToNumbers] = useState(false);
-
-    function generateMatrix(name) {
-        let newCheckboxValues = {...states['checkboxValues']};
-        newCheckboxValues[name] = !states['checkboxValues'][name];
-        states['checkboxValues'] = newCheckboxValues; 
-        stateSetter('checkboxValues', newCheckboxValues);
-
-        const selection = Object.keys(states['checkboxValues']).filter(key => states['checkboxValues'][key]);
-        let indices = new Set(selection.map(name => [...inputOptions, ...outputOptions].indexOf(name)));
-        
-        const newInputData = allInputData.filter((_, index) => indices.has(index));
-        states['inputData'] = newInputData; 
-        stateSetter('inputData', newInputData);
-        const newFeatures = inputOptions.filter((_, index) => indices.has(index));
-        states['features'] = newFeatures; 
-        stateSetter('features', newFeatures);
-
-        const newOutputData = allOutputData.filter((_, index) => indices.has(index + allInputData.length));
-        states['outputData'] = newOutputData; 
-        stateSetter('outputData', newOutputData);
-        const newTargets = outputOptions.filter((_, index) => indices.has(index + inputOptions.length));
-        states['targets'] = newTargets; 
-        stateSetter('targets', newTargets);
-
-        console.log(states['features'].length, states['features'], states['targets'])  // TODO remove
-    }
-
-    function displayMatrix(data, rowNames) {
-        return (
-            <tbody>
-                {data[0] && 
-                    <tr key={0}>
-                        <td key={0}/>
-                        {data[0].map((_, j) => (
-                            <td key={j+1} style={{ border: '1px solid black', padding: '5px' }}><i>{'Object '+String(j+1)}</i></td>
-                        ))}
-                    </tr>
-                }
-                {data[0] && data[0][0] && data.map((row, i) => (
-                    <tr key={i+1}>
-                        <td key={0} style={{ border: '1px solid black', padding: '5px' }}><i>{rowNames[i]}</i></td>
-                        {row.map((cell, j) => (
-                            <td key={j+1} style={{ border: '1px solid black', padding: '5px' }}>{cell}</td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-        )
-    }
-
-    function checkboxPosition(index) {
-        const textHeight = 20
-        return Math.round(0.12*height + 2.0*textHeight*index)
-    }
-
-    return (
-        <Box style={{ flex:1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: window.innerHeight-52, padding:'30px 50px',  }}>
-            <Flex direction='column' gap="0" style={{ alignItems: 'center', justifyContent: 'center' }}>
-            
-                {/* Display the checkboxes in two columns*/}
-                <Flex direction='row'>
-                    <Flex direction='column'>
-                        <div style={{ position: 'absolute', top: checkboxPosition(-1), left: Math.round(0.1 * width)}}>Desired Inputs:</div>
-                        {inputOptions.map((name, index) => (
-                            <Text className={name+'Checkbox'} as="label" size="2">
-                                <Flex style={{ position:"absolute", top: checkboxPosition(index), left: Math.round(0.1 * width)}}>          
-                                    <Checkbox style={{ marginRight: '10px' }} onClick={() => generateMatrix(name)} checked={states['checkboxValues'][name]} /> {name}
-                                </Flex>
-                            </Text>
-                        ))}
-                    </Flex>
-
-                    <Flex direction='column'>
-                        <div style={{ position: 'absolute', top: checkboxPosition(-1), left: Math.round(0.6 * width)}}>Desired Outputs:</div>
-                        {outputOptions.map((name, index) => (
-                            <Text className={name+'Checkbox'} as="label" size="2">
-                                <Flex style={{ position:"absolute", top: checkboxPosition(index), left: Math.round(0.6 * width)}}>          
-                                    <Checkbox style={{ marginRight: '10px' }} onClick={() => generateMatrix(name)} checked={states['checkboxValues'][name]} /> {name}
-                                </Flex>
-                            </Text>
-                        ))}
-                    </Flex>
-                </Flex>
-                
-                {/* Display the matrices */}
-                <Flex direction='column' style={{ position:'absolute', top: 0.40*height }}>
-                    <div style={{textAlign: 'center'}} >Input matrix</div>
-                    <table style={{ borderCollapse: 'collapse', textAlign: 'center' }}>
-                        {displayMatrix(states['inputData'], states['features'])}
-                    </table>
-                </Flex>
-
-                <Flex direction='column' style={{ position:'absolute', top: 0.75*height }}>
-                <div style={{textAlign: 'center'}} >Output matrix</div>
-                    <table style={{ borderCollapse: 'collapse', textAlign: 'center' }}>
-                        {displayMatrix(states['outputData'], states['targets'])}
-                    </table>
-                </Flex>
-
-            </Flex>
-        </Box>
-    );
-
-    /* Checkboxes */
-
-
-    /* Input Matrix */
 }
 
 export default OtherTask;
---
+++ src/utils/otherTasks/plottingUtils.js
@@ -0,0 +1,223 @@
+import '../../css/App.css';
+import React, {  } from 'react';
+import * as Slider from '@radix-ui/react-slider';
+import { Flex, Box } from '@radix-ui/themes';
+import { Chart, registerables } from 'chart.js';
+//import { Chart, CategoryScale, LinearScale, LineController, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+
+function throttle(func, limit) {
+    let inThrottle;
+    return function(...args) {
+        if (!inThrottle) {
+            func.apply(this, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
+
+function debounce(func, delay) {
+    let debounceTimer;
+    return function(...args) {
+      const context = this;
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+}
+
+// General-purpose handleChange function, call like this:
+//      const processValue = () => {
+//          ...
+//      }
+//      const plotValue = () => {
+//          ...
+//      }
+//      const genericSlider = (
+//          <Slider.Root
+//             (...)
+//             onValueChange={(value) => handleChangeWrapper(value[0], processValue, plotValue, 'genericState', states, stateSetter)}  // COPY THIS LINE 
+//           >
+//              (...)
+//           </Slider.Root>
+//         );
+
+const handleChangeWrapper = (value, processingFunction, plottingFunction, state, all_states, stateSetter, delay=50) => {
+    const handleChange = throttle((value, processingFunction, plottingFunction, state, all_states) => {  // TODO: switch to throttle while user is dragging the slider, but somehow take value the user lands on
+        if (processingFunction) {value = processingFunction(value)}
+        
+        stateSetter(state, value);  
+        all_states[state] = value
+    
+        plottingFunction(value)
+    
+        return all_states
+    }, delay);
+
+    handleChange(value, processingFunction, plottingFunction, state, all_states)
+}
+
+function meanSquaredError(a, x, b, y) {  // TODO: Copilot-generated, check if it works
+    let sum = 0;
+    for (let i = 0; i < x.length; i++) {
+        sum += Math.pow((a * x[i] + b - y[i]), 2);
+    }
+    return sum / x.length;
+}
+
+Chart.register(...registerables);
+let chartInstance = null
+function makeScatterChart(ctx, x, y) {  // TODO: Copilot-generated, check if this works
+    const scatterData = {
+        datasets: [{
+            label: 'Scatter Dataset',
+            data: x.map((xi, index) => ({ x: xi, y: y[index] })),
+            backgroundColor: 'rgba(4, 151, 185, 1)'
+        }]
+    };
+
+    const scatterOptions = {
+        scales: {
+            x: {
+                min: -10,
+                max: 10
+            },
+            y: {
+                min: -10,
+                max: 10
+            }
+        }
+    };
+
+    if (chartInstance) {
+        chartInstance.destroy()
+    }
+
+    chartInstance =  new Chart(ctx, {
+        type: 'scatter',
+        data: scatterData,
+        options: scatterOptions
+    });
+
+    return chartInstance
+}
+
+export const renderLinReg = (width, height, states, stateSetter) => {  // width & height are for the bounding box of the animation (the right side of the vertical separator)
+    // const ws = new WebSocket(`wss://${this.props.host}/ws/${this.props.userId}/`);
+    const chartRef = React.createRef();  // plotting is weird
+
+    // TODO: for whatever reason this broke everything
+    // if (!(states['weight'] && states['bias'])) {
+    //     stateSetter('weight', 45);
+    //     states['weight'] = 45;
+    //     stateSetter('bias', 0);
+    //     states['bias'] = 0
+    // }
+
+    if (!(states['x'] && states['y'])) {  // TODO: check if this works
+        const target_a = Math.tan((Math.random()/3)*Math.PI).toFixed(3)
+        const target_b = Math.floor(Math.random() * 12 - 5).toFixed(3)
+        const x = Array.from({ length: 100 }, () => Math.floor(Math.random() * 20) - 10);
+        const y = x.map(xi => target_a * xi + parseFloat(target_b) + (Math.random() * 2.82 - 1.41));  // approximate noise as a normal distribution
+        stateSetter('x', x)
+        states['x'] = x
+        stateSetter('y', y)
+        states['y'] = y
+    }
+
+    const plotData = (weight, bias) => {  // TODO: Copilot-generated, chack if this works
+        const scatterChart = makeScatterChart(chartRef.current, states['x'], states['y'])
+
+        if (weight !== null && bias !== null) {
+            const lineData = {
+                datasets: [{
+                    label: 'Line Dataset',
+                    data: Array.from({ length: 200 }, (_, i) => {
+                        const x_s = -10 + (20 / 199) * i;
+                        return { x: x_s, y: weight * x_s + bias };
+                    }),
+                    borderColor: 'rgba(185, 38, 4, 1)',
+                    fill: false,
+                    type: 'line'
+                }]
+            };
+
+            scatterChart.data.datasets.push(lineData.datasets[0]);
+            scatterChart.update();
+
+            stateSetter('error', meanSquaredError(weight, states['x'], bias, states['y']).toFixed(3))
+            console.log(weight, bias)  // TODO remove
+        }
+    }
+    
+    const processWeight = (value) => {
+        value = value * Math.PI / 180;
+        value = Math.tan(value);
+        value = parseFloat(value.toFixed(3))
+        return value
+    }
+
+    const plottingWrapper = (value) => {
+        plotData(states['weight'], states['bias'])
+    }
+
+    const weightSlider = (
+        <Slider.Root
+            className="SliderRoot"
+            defaultValue={[45]}
+            onValueChange={(value) => handleChangeWrapper(value[0], processWeight, plottingWrapper, 'weight', states, stateSetter)}
+            min={-85}
+            max={85}
+            step={1}
+            style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+        >
+            <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+                <Slider.Range className="SliderRange" />
+            </Slider.Track>
+            <Slider.Thumb className="SliderThumb" aria-label="Weight" />
+        </Slider.Root>
+    );
+
+    const biasSlider = (
+        <Slider.Root
+            className="SliderRoot"
+            defaultValue={[0]}
+            onValueChange={(value) => handleChangeWrapper(value[0], null, plottingWrapper, 'bias', states, stateSetter)}  // COPY THIS LINE
+            min={-5}
+            max={5}
+            step={0.01}
+            style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+        >
+            <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+                <Slider.Range className="SliderRange" />
+            </Slider.Track>
+            <Slider.Thumb className="SliderThumb" aria-label="Bias" />
+        </Slider.Root>
+    );  
+
+    return (
+        <Box style={{ flex:1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: window.innerHeight-52, padding:'30px 50px' }}>
+            <Flex direction='column' gap="0" style={{ alignItems: 'center', justifyContent: 'center' }}>
+            
+                <div>Weight: {states['weight']}</div>
+        
+                {weightSlider}
+
+                <div>Bias: {states['bias']}</div>
+                <div className="slider" style={{ marginTop:10   , height:50, display: 'flex', justifyContent: 'center' }}>
+                    {biasSlider}
+                </div>
+                
+                {chartRef ? 
+                <Box style={{ height: window.innerHeight*0.55, marginBottom:10 }}>
+                    <canvas ref={chartRef}> </canvas>
+                </Box>
+                : null}
+                
+                <div>
+                    Current error: {states['error']}
+                </div>
+
+            </Flex>
+        </Box>
+    );
+}

6503aa793fd09a51ad71ed3dca9b4d0ff3ae2570
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2025-03-05 16:13:52 +0100

    Trying to fix disappearing plot

--- src/newBuildView.js
+++ src/newBuildView.js
@@ -272,7 +272,7 @@ class Building extends Model {
     }
     
     handleStartClick = (() => {
-    this.setState({ showCode: true })  // maybe this will bring the db plot back? 
+    this.setState({ showCode: false })  // maybe this will bring the db plot back? 
     let inThrottle;
     return (event) => {
         if (!inThrottle && this.props.taskId !== 0) { 
